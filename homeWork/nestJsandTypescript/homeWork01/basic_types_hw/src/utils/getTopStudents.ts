3cabd2249dd7a4e07f00265f372080f058bef413
atanasovv93 <nikolaatanasov93@yahoo.com>
2025-05-08 17:12:29 +0200

    students without grades fixed

--- homeWork/nestJsandTypescript/homeWork01/basic_types_hw/src/utils/getTopStudents.ts
+++ homeWork/nestJsandTypescript/homeWork01/basic_types_hw/src/utils/getTopStudents.ts
@@ -2,10 +2,12 @@ import { Course } from "../models/Course";
 
 export function getTopStudents(course: Course, n: number) {
   return course.students
-    .map(student => ({
-      ...student,
-      avg: student.grades.reduce((a, b) => a + b, 0) / student.grades.length
-    }))
+    .map(student => {
+      const avg = student.grades.length > 0 
+        ? student.grades.reduce((a, b) => a + b, 0) / student.grades.length
+        : 0; 
+      return { ...student, avg };
+    })
     .sort((a, b) => b.avg - a.avg)
-    .slice(0, n);
+    .slice(0, n); 
 }
